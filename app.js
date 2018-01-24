9e53e0392c998fa1c76e93ba16e838267d58b612
arthur <arthur.osipenko@gmail.com>
2018-01-24 18:15:37 +0200

    add simple server

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-'use strict';
-
-let express = require('express');
-let path = require('path');
-let app = express();
-let compress = require('compression');
-let cors = require('cors');
-let helmet = require('helmet');
-let port = process.env.PORT || 7301;
-let hostname = process.env.HOST || "localhost";
+const express = require('express');
+const path = require('path');
+const app = express();
+const compress = require('compression');
+const cors = require('cors');
+const helmet = require('helmet');
+const port = process.env.PORT || 7301;
+const hostname = process.env.HOST || 'localhost';
 
 app.use(helmet());
-app.use(compress({threshold: '16k'}));
+app.use(compress({ threshold: '16k' }));
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'build/')));
 
 app.get('*', (req, res) => {
-  res.sendFile("index.html", {root: path.join(__dirname, 'build/')});
+  res.sendFile('index.html', { root: path.join(__dirname, 'build/') });
 });
 
 app.listen(port, hostname, () => {
