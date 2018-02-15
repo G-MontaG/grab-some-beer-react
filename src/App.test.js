6c29a9f4913032cbd4c17f013b71ecc453f778ea
arthur <arthur.osipenko@gmail.com>
2018-02-15 12:59:02 +0200

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 
-it('renders welcome message', () => {
-  const wrapper = shallow(<Provider store={store}><App /></Provider>);
-  // expect(wrapper.contains(welcome)).to.equal(true);
-  expect(wrapper.find('.App')).toHaveLength(1);
+describe('<App />', () => {
+  let wrapper = null;
+  beforeEach(() => {
+    wrapper = shallow(<App />, { context: { store } }).dive();
+  });
+
+  it('renders the dumb component', () => {
+    expect(wrapper.length).toEqual(1);
+  });
+
+  it('contains app root div', () => {
+    expect(wrapper.find('.App')).toHaveLength(1);
+  });
 });
